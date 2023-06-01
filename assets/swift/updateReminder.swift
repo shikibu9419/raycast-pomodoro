1dced9b049310c5eed9cf5b2a20766fc5e943813
Kazuya Izumi <shikibu9419@gmail.com>
2023-06-01 14:10:36 +0900

    Fix update reminder bug

--- assets/swift/updateReminder.swift
+++ assets/swift/updateReminder.swift
@@ -1,23 +1,22 @@
 import Foundation
 import EventKit
 
-func updateReminder(in eventStore: EKEventStore, listName: String, reminderId: String, reminderJson: [String: Any]) {
+func updateReminder(in eventStore: EKEventStore, reminderId: String, reminderJson: [String: Any]) {
     let calendars = eventStore.calendars(for: .reminder)
+    for calendar in calendars {
+        let predicate = eventStore.predicateForReminders(in: [calendar])
+        let dispatchGroup = DispatchGroup()
+        dispatchGroup.enter()
+        var reminders: [EKReminder]?
+        eventStore.fetchReminders(matching: predicate) { fetchedReminders in
+            reminders = fetchedReminders
+            dispatchGroup.leave()
+        }
+        dispatchGroup.wait()
 
-    // Find the calendar with the specified list name
-    guard let calendar = calendars.first(where: { $0.title == listName }) else {
-        printError("Could not find list with name \(listName)")
-        return
-    }
-
-    let predicate = eventStore.predicateForReminders(in: [calendar])
-    let dispatchGroup = DispatchGroup()
-    dispatchGroup.enter()
-    eventStore.fetchReminders(matching: predicate) { reminders in
-        guard let reminders = reminders else { return }
-        if let reminder = reminders.first(where: { $0.calendarItemIdentifier == reminderId }) {
-            if let completed = reminderJson["completed"] as? Bool {
-                reminder.isCompleted = completed
+        if let reminder = reminders?.first(where: { $0.calendarItemIdentifier == reminderId }) {
+            if let completed = reminderJson["completed"] as? Int {
+                reminder.isCompleted = completed == 1
             }
             if let title = reminderJson["title"] as? String {
                 reminder.title = title
@@ -40,26 +39,23 @@ func updateReminder(in eventStore: EKEventStore, listName: String, reminderId: S
         } else {
             printError("Could not find reminder with ID \(reminderId)")
         }
-        dispatchGroup.leave()
     }
-    dispatchGroup.wait()
 }
 
 // Command line arguments handling
 let args = CommandLine.arguments
 
-if args.count != 4 {
-    printError("Invalid number of arguments. Expected 3, got \(args.count - 1).")
+if args.count != 3 {
+    printError("Invalid number of arguments. Expected 2, got \(args.count - 1).")
     exit(1)
 }
 
-let listName = args[1]
-let reminderId = args[2]
-let reminderJsonData = args[3].data(using: .utf8)!
+let reminderId = args[1]
+let reminderJsonData = args[2].data(using: .utf8)!
 
 guard let reminderJson = try? JSONSerialization.jsonObject(with: reminderJsonData, options: []) as? [String: Any] else {
     printError("Invalid JSON format.")
     exit(1)
 }
 
-updateReminder(in: EKEventStore(), listName: listName, reminderId: reminderId, reminderJson: reminderJson)
+updateReminder(in: EKEventStore(), reminderId: reminderId, reminderJson: reminderJson)
