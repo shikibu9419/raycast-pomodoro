6cc3ebd837aa78f5a45eca8926fdb55f3f3a9a46
Kazuya Izumi <shikibu9419@gmail.com>
2023-06-04 11:36:23 +0900

    Update swift code

--- assets/swift/utils.swift
+++ assets/swift/utils.swift
@@ -1,13 +1,12 @@
+let dateFormatter = ISO8601DateFormatter()
+dateFormatter.timeZone = TimeZone(identifier: "Asia/Tokyo")!
+
 func dateFromString(_ dateString: String) -> Date? {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd"
     return dateFormatter.date(from: dateString)
 }
 
 func stringFromDate(_ date: Date?) -> String {
     guard let date = date else { return "" }
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-ddTHH:mm:ss"
     return dateFormatter.string(from: date)
 }
 
